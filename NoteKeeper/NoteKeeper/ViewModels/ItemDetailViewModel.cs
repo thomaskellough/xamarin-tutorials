d3a778db84af7b7cc872a6092ec5ea7a8aa1dd37
Thomas Kellough <thomas.r.kellough@gmail.com>
2020-05-03 22:42:31 -0500

    init commit - NoteKeeper

---
+++ NoteKeeper/NoteKeeper/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using NoteKeeper.Models;
+
+namespace NoteKeeper.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Note Note { get; set; }
+        public IList<String> CourseList { get; set; }
+
+        public String NoteHeading
+        {
+            get { return Note.Heading; }
+            set
+            {
+                Note.Heading = value;
+                OnPropertyChanged();
+            }
+
+        }
+
+        public String NoteText
+        {
+            get { return Note.Text; }
+            set
+            {
+                Note.Text = value;
+                OnPropertyChanged();
+            }
+
+        }
+
+        public String NoteCourse
+        {
+            get { return Note.Course; }
+            set
+            {
+                Note.Course = value;
+                OnPropertyChanged();
+            }
+
+        }
+
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            InitializeCourseList();
+            Note = new Note
+            {
+                Heading = "Test note",
+                Text = "Text for note in ViewModel",
+                Course = CourseList[0]
+            };
+        }
+
+        async void InitializeCourseList()
+        {
+            CourseList = await PluralsightDataStore.GetCoursesAsync();
+        }
+    }
+}
---
+++ NoteKeeper/NoteKeeper/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using NoteKeeper.Models;
+using NoteKeeper.ViewModels;
+using System.Collections.Generic;
+using NoteKeeper.Services;
+
+namespace NoteKeeper.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        ItemDetailViewModel viewModel;
+
+        public ItemDetailPage(ItemDetailViewModel viewModel)
+        {
+            InitializeComponent();
+
+            this.viewModel = viewModel;
+            BindingContext = this.viewModel;
+        }
+
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+
+            viewModel = new ItemDetailViewModel();
+            BindingContext = viewModel;
+        }
+
+        public void Cancel_Clicked(object sender, EventArgs eventArgs)
+        {
+            viewModel.NoteHeading = "XXXXXXX";
+            DisplayAlert("Cancel option",
+                "Heading value is " + viewModel.Note.Heading,
+                "Button 2", "Button 1");
+        }
+
+        public void Save_Clicked(object sender, EventArgs eventArgs)
+        {
+            DisplayAlert("Save option",
+                "Save was clicked",
+                "Button 2", "Button 1");
+        }
+
+    }
+}
