2596811cf4cad84ca78b93f19ea801d568c3c0e0
Thomas Kellough <thomas.r.kellough@gmail.com>
2020-05-04 19:49:49 -0500

    added itempage

--- NoteKeeper/NoteKeeper/ViewModels/ItemsViewModel.cs
+++ NoteKeeper/NoteKeeper/ViewModels/ItemsViewModel.cs
@@ -12,34 +12,50 @@ namespace NoteKeeper.ViewModels
 {
     public class ItemsViewModel : BaseViewModel
     {
-        public ObservableCollection<Item> Items { get; set; }
+        public ObservableCollection<Note> Notes { get; set; }
         public Command LoadItemsCommand { get; set; }
 
         public ItemsViewModel()
         {
             Title = "Browse";
-            Items = new ObservableCollection<Item>();
+            Notes = new ObservableCollection<Note>();
             LoadItemsCommand = new Command(async () => await ExecuteLoadItemsCommand());
 
-            MessagingCenter.Subscribe<NewItemPage, Item>(this, "AddItem", async (obj, item) =>
-            {
-                var newItem = item as Item;
-                Items.Add(newItem);
-                await DataStore.AddItemAsync(newItem);
-            });
+            // Handle "SaveNote" message 
+            MessagingCenter.Subscribe<ItemDetailPage, Note>(this, "SaveNote",
+                async (sender, note) => {
+                    // Add note to collection - will automatically refresh data binding
+                    Notes.Add(note);
+                    // Add to data store
+                    await PluralsightDataStore.AddNoteAsync(note);
+                });
+
+            // Handle "UpdateNote" message 
+            MessagingCenter.Subscribe<ItemDetailPage, Note>(this, "UpdateNote",
+                async (sender, note) => {
+                    // Update note in data store
+                    await PluralsightDataStore.UpdateNoteAsync(note);
+                    // Modifying a member (our note) within an ObservableCollection
+                    //  does not automatically refresh data binding .. so explicitly
+                    //  repopulate the collection
+                    await ExecuteLoadItemsCommand();
+                });
         }
 
         async Task ExecuteLoadItemsCommand()
         {
+            if (IsBusy)
+                return;
+
             IsBusy = true;
 
             try
             {
-                Items.Clear();
-                var items = await DataStore.GetItemsAsync(true);
-                foreach (var item in items)
+                Notes.Clear();
+                var notes = await PluralsightDataStore.GetNotesAsync();
+                foreach (var note in notes)
                 {
-                    Items.Add(item);
+                    Notes.Add(note);
                 }
             }
             catch (Exception ex)
--- NoteKeeper/NoteKeeper/Views/ItemsPage.xaml.cs
+++ NoteKeeper/NoteKeeper/Views/ItemsPage.xaml.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -13,9 +13,6 @@ using NoteKeeper.ViewModels;
 
 namespace NoteKeeper.Views
 {
-    // Learn more about making custom code visible in the Xamarin.Forms previewer
-    // by visiting https://aka.ms/xamarinforms-previewer
-    [DesignTimeVisible(false)]
     public partial class ItemsPage : ContentPage
     {
         ItemsViewModel viewModel;
@@ -27,11 +24,17 @@ namespace NoteKeeper.Views
             BindingContext = viewModel = new ItemsViewModel();
         }
 
-        async void OnItemSelected(object sender, EventArgs args)
+        async void OnItemSelected(object sender, SelectedItemChangedEventArgs args)
         {
-            var layout = (BindableObject)sender;
-            var item = (Item)layout.BindingContext;
-            await Navigation.PushAsync(new ItemDetailPage(new ItemDetailViewModel(item)));
+            var note = args.SelectedItem as Note;
+            if (note == null)
+                return;
+
+            await Navigation.PushModalAsync(
+                new NavigationPage(new ItemDetailPage(new ItemDetailViewModel(note))));
+
+            // Manually deselect item.
+            ItemsListView.SelectedItem = null;
         }
 
         async void AddItem_Clicked(object sender, EventArgs e)
@@ -43,8 +46,8 @@ namespace NoteKeeper.Views
         {
             base.OnAppearing();
 
-            if (viewModel.Items.Count == 0)
-                viewModel.IsBusy = true;
+            if (viewModel.Notes.Count == 0)
+                viewModel.LoadItemsCommand.Execute(null);
         }
     }
 }
